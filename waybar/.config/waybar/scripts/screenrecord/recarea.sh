478ea9f3e3bb7216861099bea6a697bf6e23c98d
bossearch <yudha.bosse@gmail.com>
2024-09-22 13:15:33 +0800

    more adjustment

--- waybar/.config/waybar/scripts/screenrecord/recarea.sh
+++ waybar/.config/waybar/scripts/screenrecord/recarea.sh
@@ -18,15 +18,15 @@ if [ -z "$geometry" ]; then
   exit 1
 fi
 
+# Create or update tooltip file for checkrec.sh
+echo "Screenrecording on Area: $geometry" >$tooltip
+
 # Notify the user that the recording has started
 dunstify "Screenrecording on Area: $geometry will start in 3 seconds" -t 2500
 sleep 3
 
 # Start recording the entire monitor
 wl-screenrec --audio --low-power=off --no-damage -g "$geometry" -f "$filename"
-
-# Create or update tooltip file for checkrec.sh
-echo "Screenrecording on Area: $geometry" >$tooltip
 
 # When the script finishes (e.g., with CTRL+C), notify the user
 trap 'dunstify "Screenrecord saved to $filename" -t 3000' EXIT
--- waybar/.config/waybar/scripts/screenrecord/recmonitor.sh
+++ waybar/.config/waybar/scripts/screenrecord/recmonitor.sh
@@ -12,15 +12,15 @@ filename=~/Videos/Screenrecords/$output-$(date +%F_%T).mp4
 # Define tooltip file location
 tooltip=~/.config/waybar/scripts/screenrecord/tooltip
 
+# Create or update tooltip file for checkrec.sh
+echo "Screenrecording on Monitor: $output" >$tooltip
+
 # Notify the user that the recording has started
 dunstify "Screenrecording on Monitor: $output will start in 3 seconds" -t 2500
 sleep 3
 
 # Start recording the entire monitor
 wl-screenrec --audio --low-power=off --no-damage -o "$output" -f "$filename"
-
-# Create or update tooltip file for checkrec.sh
-echo "Screenrecording on Monitor: $output" >$tooltip
 
 # When the script finishes (e.g., with CTRL+C), notify the user
 trap 'dunstify "Screenrecord saved to $filename" -t 3000' EXIT
