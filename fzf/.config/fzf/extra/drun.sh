72c49e244daef43529858bfd1cd82357650be96d
bossearch <yudha.bosse@gmail.com>
2025-01-14 07:57:20 +0800

    update

--- fzf/.config/fzf/extra/drun.sh
+++ fzf/.config/fzf/extra/drun.sh
@@ -1,34 +1,30 @@
 #!/bin/sh
 
-# List all .desktop files from standard and Flatpak directories
-applications=$(ls /usr/share/applications/*.desktop \
-  ~/.local/share/applications/*.desktop \
-  /var/lib/flatpak/app/*/current/active/export/share/applications/*.desktop \
-  2>/dev/null)
+# Read the app names and Exec commands from the saved txt file
+apps_list=$(cat $HOME/.config/fzf/extra/assets/drun.txt)
 
-# Use basename to only show filenames without extension in fzf
-selected=$(echo "$applications" | xargs -I{} basename {} | sed 's/\.desktop$//' | sort | \
-  fzf -m --layout=default --border=none --highlight-line --info=inline-right --prompt="❯ "\
-  --color=fg:#c0caf5,fg+:#B1BAE4,bg:#1a1b26,bg+:#16161e,hl:#92BE67,hl+:#93d84e\
-  --color=info:#7296E6,marker:#E26E87,prompt:#AC8FE6,spinner:#E26E87,pointer:#E26E87\
-  --color=gutter:#1a1b26,separator:#27a1b9,scrollbar:#27a1b9,label:#aeaeae,query:#B1BAE4\
-  --bind "enter:accept"\
+# Use fzf to list app names and select one
+selected=$(
+  echo "$apps_list" | cut -d'|' -f1 | sort |
+    fzf -m --layout=default --border=none --highlight-line --info=inline-right --prompt="❯ " \
+      --color=fg:#c0caf5,fg+:#B1BAE4,bg:#1a1b26,bg+:#16161e,hl:#92BE67,hl+:#93d84e \
+      --color=info:#7296E6,marker:#E26E87,prompt:#AC8FE6,spinner:#E26E87,pointer:#E26E87 \
+      --color=gutter:#1a1b26,separator:#27a1b9,scrollbar:#27a1b9,label:#aeaeae,query:#B1BAE4 \
+      --bind "enter:accept"
 )
 
-# Process each selected .desktop file
-while IFS= read -r file; do
-  if [ -n "$file" ]; then
-    # Find the full path of the selected .desktop file
-    full_path=$(echo "$applications" | grep "/$file.desktop$")
+# Run the selected application's Exec command
+if [ -n "$selected" ]; then
+  # Get the corresponding Exec command from the list
+  exec_command=$(echo "$apps_list" | grep "^$selected|" | cut -d'|' -f2)
 
-    # Extract the command from the Exec line in the .desktop file
-    extract=$(grep '^Exec=' "$full_path" | tail -1 | sed 's/^Exec=//' | sed 's/%.*//')
+  drun="setsid $exec_command >/dev/null 2>&1 &"
+  eval "$drun"
 
-    # Run the command
-    #echo "$extract" | xargs -r -I {} sh -c 'nohup {} >/dev/null 2>&1 & disown'
-    echo "$extract" | xargs -r -I {} sh -c 'sleep 0.1; nohup {} > /tmp/launcher_output.log 2>&1 & disown'
+  if echo "$exec_command" | grep -q "pkexec"; then
+    # Delay execution to allow pkexec to process properly
+    sleep 1
+  else
+    :
   fi
-done <<< "$selected"
-
-echo "Selected: $selected" >> /tmp/launcher_debug.log
-echo "Command: $extract" >> /tmp/launcher_debug.log
+fi
