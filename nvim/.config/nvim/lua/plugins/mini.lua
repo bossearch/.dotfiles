7656cfe9351290270d89c14370d2ad49b2d26406
bossearch <yudha.bosse@gmail.com>
2025-01-19 13:08:01 +0800

    update

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,52 +1,22 @@
 return {
-  {
-    "echasnovski/mini.pairs",
-    version = "*",
-    config = function()
-      -- In which modes mappings from this `config` should be created
-      require("mini.pairs").setup({
-        modes = { insert = true, command = false, terminal = false },
+  "echasnovski/mini.nvim",
+  config = function()
+    require("mini.icons").setup()
+    require("mini.pairs").setup({
+      modes = { insert = true, command = true, terminal = true },
+      mappings = {
+        ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+        ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+        ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
 
-        -- Global mappings. Each right hand side should be a pair information, a
-        -- table with at least these fields (see more in |MiniPairs.map|):
-        -- - <action> - one of 'open', 'close', 'closeopen'.
-        -- - <pair> - two character string for pair to be used.
-        -- By default pair is not inserted after `\`, quotes are not recognized by
-        -- `<CR>`, `'` does not insert pair after a letter.
-        -- Only parts of tables can be tweaked (others will use these defaults).
-        mappings = {
-          ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
-          ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
-          ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+        [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+        ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+        ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
 
-          [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
-          ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
-          ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
-
-          ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
-          ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
-          ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
-        },
-      })
-    end,
-  },
-  {
-    "echasnovski/mini.icons",
-    lazy = true,
-    opts = {
-      file = {
-        [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
-        ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+        ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
       },
-      filetype = {
-        dotenv = { glyph = "", hl = "MiniIconsYellow" },
-      },
-    },
-    init = function()
-      package.preload["nvim-web-devicons"] = function()
-        require("mini.icons").mock_nvim_web_devicons()
-        return package.loaded["nvim-web-devicons"]
-      end
-    end,
-  },
+    })
+  end,
 }
