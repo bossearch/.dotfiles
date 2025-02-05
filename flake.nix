e9796f5bc5104ad6199848b26780b7d6686dcf9f
bossearch <yudha.bosse@gmail.com>
2025-02-05 11:52:49 +0800

    fix yazi

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs"; # Keep Home Manager in sync with nixpkgs
   };
@@ -10,9 +11,10 @@
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
     home-manager,
     ...
-  }: let
+  }@inputs: let
     lib = nixpkgs.lib;
     system = "x86_64-linux"; # Change this if you're using ARM (e.g., a Raspberry Pi)
   in {
@@ -26,6 +28,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.bosse = import ./hosts/desktop/home.nix; # User config
           }
         ];
