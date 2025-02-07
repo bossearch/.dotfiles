8f36b39410716c0a6d9419f707791c2f23f27016
bossearch <yudha.bosse@gmail.com>
2025-02-07 15:46:41 +0800

    fix tmux & remove gitignore

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -3,8 +3,11 @@
   home.packages = with pkgs; [
     tmux
   ];
-  home.file.".config/tmux" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/configs/tmux";
+  home.file.".config/tmux/tmux.conf" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/configs/tmux/tmux.conf";
+  };
+  home.file.".config/tmux/plugins/tpm" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/configs/tmux/tpm";
     recursive = true;
   };
 }
