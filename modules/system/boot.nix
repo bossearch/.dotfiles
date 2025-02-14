e03dd058748d3f1a76817fae2d2ab298300f4b3c
bossearch <yudha.bosse@gmail.com>
2025-02-14 17:32:12 +0800

    config update for mpv and nchat, remove bios boot for vm host, nautilus module adjustment

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,34 +1,13 @@
-{
-  config,
-  lib,
-  ...
-}: let
-  hostName = {
-    vm = config.networking.hostName == "vm";
-    desktop = config.networking.hostName == "desktop";
+{...}: {
+  boot = {
+    # Use systemd bootload
+    loader.systemd-boot.enable = true;
+    loader.systemd-boot.consoleMode = "max";
+    loader.efi.canTouchEfiVariables = true;
+    loader.timeout = 1;
+    #silent boot
+    kernelParams = ["quiet" "udev.log_level=0" "systemd.show_status=false"];
+    initrd.verbose = false;
+    consoleLogLevel = 0;
   };
-in {
-  boot = lib.mkMerge [
-    (lib.mkIf hostName.desktop {
-      # Use systemd bootload
-      loader.systemd-boot.enable = true;
-      loader.systemd-boot.consoleMode = "max";
-      loader.efi.canTouchEfiVariables = true;
-      loader.timeout = 1;
-      #silent boot
-      kernelParams = ["quiet" "udev.log_level=0" "systemd.show_status=false"];
-      initrd.verbose = false;
-      consoleLogLevel = 0;
-    })
-
-    (lib.mkIf hostName.vm {
-      loader.grub.enable = true;
-      loader.grub.device = "/dev/vda"; # or "nodev" for efi only
-      loader.timeout = 1;
-      #silent boot
-      kernelParams = ["quiet" "udev.log_level=0" "systemd.show_status=false"];
-      initrd.verbose = false;
-      consoleLogLevel = 0;
-    })
-  ];
 }
--- modules/user/nautilus.nix
+++ modules/user/nautilus.nix
@@ -4,4 +4,9 @@
     sushi
     vaults
   ];
+  dconf.settings = {
+    "org/gnome/desktop/privacy" = {
+      remember-recent-files = false;
+    };
+  };
 }
