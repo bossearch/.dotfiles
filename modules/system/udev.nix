f0b37b337e42306284f751ad8860631a7542cc04
bossearch <yudha.bosse@gmail.com>
2025-02-18 23:46:40 +0800

    adjust usbguard and udev module to detect device name

--- modules/system/udev.nix
+++ modules/system/udev.nix
@@ -4,6 +4,7 @@
     export DISPLAY=:0
     export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
+    DEVICE_PATH="$1"
     LOCKFILE="/tmp/usbguard_lock_$1"  # Use device ID for unique lockfile
 
     # Prevent duplicate prompts for the same device
@@ -13,9 +14,18 @@
 
     touch "$LOCKFILE"
 
+    # Get the device name using udevadm
+    DEVICE_NAME=$(udevadm info --query=property --name="$DEVICE_PATH" | grep -i "ID_MODEL=" | cut -d= -f2)
+    DEVICE_VENDOR=$(udevadm info --query=property --name="$DEVICE_PATH" | grep -i "ID_VENDOR=" | cut -d= -f2)
+
+    # If the device name is not found, fall back to using the device path
+    if [ -z "$DEVICE_NAME" ]; then
+      DEVICE_NAME="$DEVICE_PATH"
+    fi
+
     # Show Zenity dialog
     choice=$(/run/wrappers/bin/su bosse -c "${pkgs.zenity}/bin/zenity --question \
-      --text='Do you trust this HID device?' --title='New Device Detected' \
+      --text='Do you trust the $DEVICE_VENDOR $DEVICE_NAME device?' --title='New Device Detected' \
       --ok-label='Allow' --cancel-label='Block'")
 
     if [ $? -eq 0 ]; then
