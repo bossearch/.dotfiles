9917ab808533756bd7f44b02a1c5dfea3b4e9299
bossearch <yudha.bosse@gmail.com>
2025-02-22 15:41:59 +0800

    adjust ssh & network modules

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -1,9 +1,8 @@
-{...}: {
+{
+  ...
+}: {
   networking = {
     nameservers = ["127.0.0.1" "::1"];
-    # If using dhcpcd:
-    dhcpcd.extraConfig = "nohook resolv.conf";
-    # If using NetworkManager:
     networkmanager.dns = "none";
   };
 
--- modules/system/ssh.nix
+++ modules/system/ssh.nix
@@ -2,8 +2,8 @@
   services.openssh = {
     enable = lib.mkForce true;
     settings = {
-      PasswordAuthentication = false; # Disable password authentication (recommended for security)
-      PermitRootLogin = "prohibit-password"; # Disallow root login over SSH
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
     };
   };
 }
