a90c9248eb651d5f7fcbe013fde0df552a281f4a
bossearch <yudha.bosse@gmail.com>
2025-03-07 05:02:41 +0800

    fix vaults dir for nautilus bookmark

--- modules/user/nautilus.nix
+++ modules/user/nautilus.nix
@@ -50,14 +50,13 @@ in {
     file:///home/bosse/Documents Documents
     file:///home/bosse/Pictures Pictures
     file:///home/bosse/Videos Videos
-    file:///home/bosse/Vaults Vaults
+    file:///home/bosse/.local/share/Vaults Vaults
   '';
 
   home.file.".config/user-dirs.dirs".text = ''
     XDG_DESKTOP_DIR="$HOME/Desktop"
     XDG_DOCUMENTS_DIR="$HOME/Documents"
     XDG_DOWNLOAD_DIR="$HOME/Downloads"
-    XDG_MUSIC_DIR="$HOME/Music"
     XDG_PICTURES_DIR="$HOME/Pictures"
     XDG_VIDEOS_DIR="$HOME/Videos"
   '';
