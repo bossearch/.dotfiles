c107910152199f228cf03aa833aaf2eb6a6a7e3e
bossearch <yudha.bosse@gmail.com>
2025-03-07 19:48:41 +0800

    update kitty config & disable cursor_trail for some scripts

--- configs/waybar/scripts/launcher/launcher-right.sh
+++ configs/waybar/scripts/launcher/launcher-right.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 hyprctl dispatch movecursor 1280 720
-kitty -T mousefzf -o font_size=24 "$HOME"/.config/fzf/extra/drun.sh
+kitty -T mousefzf -o font_size=24 -o cursor_trail=0 "$HOME"/.config/fzf/extra/drun.sh
