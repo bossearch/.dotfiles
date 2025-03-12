d31ea4085d25172f93cae046aa24fd2f62b8dd31
bossearch <yudha.bosse@gmail.com>
2025-03-13 03:03:59 +0800

    add new module for neomutt

--- hosts/vm/home.nix
+++ hosts/vm/home.nix
@@ -18,6 +18,7 @@
     # ./../../modules/user/mpv.nix
     # ./../../modules/user/nautilus.nix
     # ./../../modules/user/nchat.nix
+    # ./../../modules/user/neomutt.nix
     # ./../../modules/user/newsboat.nix
     ./../../modules/user/nvim.nix
     # ./../../modules/user/nyaa.nix
