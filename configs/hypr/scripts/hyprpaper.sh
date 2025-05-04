fef66cccabb4a86ca575979f3c4b614c1dc474c7
bossearch <yudha.bosse@gmail.com>
2025-05-04 10:32:40 +0800

    fix(hypr): add support flexible file extensions to hyprpaper.sh

--- configs/hypr/scripts/hyprpaper.sh
+++ configs/hypr/scripts/hyprpaper.sh
@@ -3,20 +3,36 @@
 pushd ~/Pictures/gowall >/dev/null || exit
 
 DATE=$(date +%Y%m%d)
-TODAY_WALL="/home/bosse/Pictures/gowall/wall-${DATE}.jpeg"
+
+INPUT_WALL=$(find /home/bosse/Pictures/gowall -maxdepth 1 -type f -name "wall-${DATE}.*" -printf "%f\n" | head -n 1)
 OUTPUT_WALL="/tmp/hyprpaper.png"
 THEME="tokyo-dark"
 
-if [ ! -f "$TODAY_WALL" ]; then
+if [ ! -f "$INPUT_WALL" ]; then
   yes y | gowall -w
-  mv wall-"$DATE"-*.jpeg wall-"$DATE".jpeg
+  echo "wall downloaded"
+else
+  echo "wall already exist"
+fi
+
+TODAY_WALL=$(find /home/bosse/Pictures/gowall -maxdepth 1 -type f -name "wall-${DATE}-*" -printf "%f\n" | head -n 1)
+# Extract components using regex
+if [[ "$TODAY_WALL" =~ wall-([0-9]{8})-[0-9]{6}\.([a-zA-Z0-9]+)$ ]]; then
+  date_part="${BASH_REMATCH[1]}"
+  ext="${BASH_REMATCH[2]}"
+  dir=$(dirname "$TODAY_WALL")
+  RENAMED_WALL="$dir/wall-${date_part}.${ext}"
+
+  mv "$TODAY_WALL" "$RENAMED_WALL"
+  echo "TODAY_WALL renamed"
+else
+  echo "TODAY_WALL does not match expected pattern."
 fi
 
 if [ ! -f "$OUTPUT_WALL" ]; then
-  gowall convert "$TODAY_WALL" -t "$THEME" --output "$OUTPUT_WALL"
+  gowall convert "$INPUT_WALL" -t "$THEME" --output "$OUTPUT_WALL"
   cp "$OUTPUT_WALL" /tmp/hyprlock.png
 fi
 
 popd >/dev/null || exit
-
-hyprpaper
+pkill hyprpaper; hyprpaper
