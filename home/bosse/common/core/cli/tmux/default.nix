638342ec569997d8dac5ea599b0aea2078917204
bossearch <yudha.bosse@gmail.com>
2025-05-17 18:22:10 +0800

    fix(tmux): update selection mode key

--- home/bosse/common/core/cli/tmux/default.nix
+++ home/bosse/common/core/cli/tmux/default.nix
@@ -63,9 +63,10 @@
       bind - split-window -v
 
       # Selection mode
-      bind-key -T copy-mode-vi 'v' send -X begin-selection
-      bind-key -T copy-mode-vi 'y' send -X copy-selection
-      unbind -T copy-mode-vi MouseDragEnd1Pane
+      set-window-option -g mode-keys vi
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle \; send -X begin-selection
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
 
       # Fzf
       unbind Space
