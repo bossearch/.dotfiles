26a683b48efdb93cefb304aa668ef6aefea2b842
bossearch <yudha.bosse@gmail.com>
2025-05-18 23:35:48 +0800

    fix(nixvim): move deprecate config to noice
    - supress vim.tbl_islist deprecated warning
      caused by noice or nui?

--- home/bosse/common/core/nvim/plugins/noice.nix
+++ home/bosse/common/core/nvim/plugins/noice.nix
@@ -1,38 +1,43 @@
 {
-  programs.nixvim.plugins.noice = {
-    enable = true;
-    settings = {
-      cmdline = {
-        view = "cmdline"; # cmdline_popup, cmdline
-        format = {
-          conceal = false;
+  programs.nixvim = {
+    extraConfigLua = ''
+      vim.deprecate = function() end
+    '';
+    plugins.noice = {
+      enable = true;
+      settings = {
+        cmdline = {
+          view = "cmdline"; # cmdline_popup, cmdline
+          format = {
+            conceal = false;
+          };
         };
-      };
-      routes.__raw = ''
-      {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-            },
-          },
-          view = "mini",
-        }
-      }
-      '';
-      presets = {
-        bottom_search = false; # use a classic bottom cmdline for search
-        command_palette = true; # position the cmdline and popupmenu together
-        long_message_to_split = true; # long messages will be sent to a split
-      };
-      views = {
-        cmdline_popup = {
-          position = {
-            row = "50%";
-            col = "50%";
+        routes.__raw = ''
+          {
+            {
+              filter = {
+                event = "msg_show",
+                any = {
+                  { find = "%d+L, %d+B" },
+                  { find = "; after #%d+" },
+                  { find = "; before #%d+" },
+                },
+              },
+              view = "mini",
+            }
+          }
+        '';
+        presets = {
+          bottom_search = false; # use a classic bottom cmdline for search
+          command_palette = true; # position the cmdline and popupmenu together
+          long_message_to_split = true; # long messages will be sent to a split
+        };
+        views = {
+          cmdline_popup = {
+            position = {
+              row = "50%";
+              col = "50%";
+            };
           };
         };
       };
