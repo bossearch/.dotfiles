0093f241d11dd6a6805e91033ff532013d1789cb
bossearch <yudha.bosse@gmail.com>
2025-05-18 23:30:41 +0800

    fix(nixvim): refactor and update todos

--- home/bosse/common/core/nvim/default.nix
+++ home/bosse/common/core/nvim/default.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  # pkgs,
+  ...
+}: {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
     ./core
@@ -7,9 +11,11 @@
   ];
   programs.nixvim = {
     enable = true;
-    extraConfigLua = ''
-      vim.deprecate = function() end
-    '';
+    viAlias = true;
+    vimAlias = true;
+    dependencies = {
+      gh.enable = true;
+    };
     performance = {
       byteCompileLua = {
         enable = true;
@@ -17,6 +23,28 @@
         configs = true;
         plugins = true;
       };
+      combinePlugins = {
+        enable = true;
+        pathsToLink = ["/plugins"];
+        standalonePlugins = [
+          "copilot.lua"
+          "blink.cmp"
+          "nvim-treesitter"
+        ];
+      };
     };
+    # extraPlugins = let
+    #   colorful-winsep = pkgs.vimUtils.buildVimPlugin {
+    #     name = "colorful-winsep";
+    #     src = pkgs.fetchFromGitHub {
+    #       owner = "nvim-zh";
+    #       repo = "colorful-winsep.nvim";
+    #       rev = "7bbe4e1353c0fe37c98bad2758aafc410280f6b3";
+    #       sha256 = "sha256-3ZXbpUNZqLHaUWgb2wHnfQfcGkMpcRtAt8q7z0tmo1o=";
+    #     };
+    #   };
+    # in [
+    #   colorful-winsep
+    # ];
   };
 }
--- home/bosse/common/core/nvim/plugins/default.nix
+++ home/bosse/common/core/nvim/plugins/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{
   imports = [
     ./blink-cmp.nix
     # ./colorful-winsep.nix
@@ -18,32 +18,6 @@
     ./tokyonight.nix
     ./treesitter.nix
     ./which-key.nix
-    # TODO: Do i need trouble, treesj, substitute, gitsigns
+    # TODO: Do i need trouble, treesj, substitute, gitsigns, comment, typr
   ];
-
-  programs.nixvim = {
-    # NOTE: Not work when performance.combinePlugins is enabled
-    # extraPlugins = let
-    #   colorful-winsep = pkgs.vimUtils.buildVimPlugin {
-    #     name = "colorful-winsep";
-    #     src = pkgs.fetchFromGitHub {
-    #       owner = "nvim-zh";
-    #       repo = "colorful-winsep.nvim";
-    #       rev = "7bbe4e1353c0fe37c98bad2758aafc410280f6b3";
-    #       sha256 = "sha256-3ZXbpUNZqLHaUWgb2wHnfQfcGkMpcRtAt8q7z0tmo1o=";
-    #     };
-    #   };
-    # in [
-    #   colorful-winsep
-    # ];
-    performance.combinePlugins = {
-      enable = true;
-      pathsToLink = ["/plugins"];
-      standalonePlugins = [
-        "blink.cmp"
-        "oil.nvim"
-        "nvim-treesitter"
-      ];
-    };
-  };
 }
