3fc7fdd89ae0b05138df0441352632654a34941c
bossearch <yudha.bosse@gmail.com>
2025-05-20 22:32:33 +0800

    fix(nixvim): refactor mini

--- home/bosse/common/core/nvim/plugins/mini/default.nix
+++ home/bosse/common/core/nvim/plugins/mini/default.nix
@@ -14,49 +14,8 @@
       jump2d = {};
       move = {};
       icons = {};
-      pairs = {
-        modes = {
-          insert = true;
-          command = true;
-          terminal = true;
-        };
-      };
-      sessions = {
-        autoread = false; # Auto-load session if no file is given
-        autowrite = true; # Auto-save session before exiting
-
-        directory.__raw = ''vim.fn.stdpath ("state") .. "/session"''; # ~/.local/state/nvim/session
-        file = ""; # Save session as 'Session.vim' inside the directory
-
-        force = {
-          read = true;
-          write = true;
-          delete = true;
-        };
-
-        hooks = {
-          pre = {
-            read = null;
-            write = null;
-            delete = null;
-          };
-          post = {
-            read.__raw = ''
-              function ()
-                vim.notify("Session restored!")
-              end
-            '';
-            write = null;
-            delete = null;
-          };
-        };
-
-        verbose = {
-          read = false;
-          write = true;
-          delete = true;
-        };
-      };
+      pairs = import ./pairs.nix;
+      sessions = import ./sessions.nix;
     };
   };
 }
