a4a41c575439f8858deeb3c2f54676e075e1e779
bossearch <yudha.bosse@gmail.com>
2025-05-21 12:16:54 +0800

    fix(nixvim): options adjustment

--- home/bosse/common/core/nvim/core/autocmd.nix
+++ home/bosse/common/core/nvim/core/autocmd.nix
@@ -96,6 +96,20 @@
         };
         desc = "Remove trailing whitespace on save";
       }
+
+      # Remove ro from format options
+      {
+        event = ["FileType"];
+        pattern = ["*"];
+        callback = {
+          __raw = ''
+            function()
+              vim.opt_local.formatoptions:remove({ 'r', 'o' })
+            end
+          '';
+        };
+        desc = "Remove ro from format options";
+      }
     ];
   };
 }
--- home/bosse/common/core/nvim/core/options.nix
+++ home/bosse/common/core/nvim/core/options.nix
@@ -20,23 +20,15 @@
       end
     '';
     opts = {
-      completeopt = "menu,menuone,noselect";
       confirm = true; # Confirm to save changes before exiting modified buffer
-      cursorline = false; # Highlight the screen line of the cursor
       cursorcolumn = false; # Highlight the screen column of the cursor
+      cursorline = true; # Highlight the screen line of the cursor
+      cursorlineopt = "number"; # cursorline opt
       expandtab = true; # Expand <Tab> to spaces in Insert mode (local to buffer)
-      fillchars = {
-        foldopen = "";
-        foldclose = "";
-        fold = " ";
-        foldsep = " ";
-        diff = "╱";
-        eob = " ";
-      };
       foldlevel = 99; # Folds level when scrolling
       ignorecase = true; # When the search query is lower-case, match both lower and upper-case
       inccommand = "split"; # Search and replace: preview changes in quickfix list
-      jumpoptions = "view";
+      jumpoptions = "view"; # restore position when jumping
       laststatus = 2; # When to use a status line for the last window
       linebreak = true; # Wrap lines at convenient points
       list = true; # Show some invisible characters (tabs...
@@ -45,7 +37,7 @@
       pumblend = 10; # Popup blend
       pumheight = 10; # Maximum number of entries in a popup
       relativenumber = true; # Relative line numbers
-      # ruler = false; # Disable the default ruler
+      ruler = false; # Disable the default ruler
       scrolloff = 10; # Number of screen lines to show around the cursor
       shiftround = true; # Round indent
       shiftwidth = 2; # Number of spaces used for each step of (auto)indent (local to buffer)
@@ -62,7 +54,6 @@
       tabstop = 2; # Number of spaces a <Tab> in the text stands for (local to buffer)
       termguicolors = true; # Enables 24-bit RGB color in the |TUI|
       undofile = true; # Automatically save and restore undo history
-      undolevels = 10000;
       virtualedit = "block"; # Allow cursor to move where there is no text in visual block mode
       winminwidth = 5; # Minimum window width
       wrap = true; # line wrap
