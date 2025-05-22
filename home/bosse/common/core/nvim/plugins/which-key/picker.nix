9f1afde95028c8e46eefb43ad91205f410f026fe
bossearch <yudha.bosse@gmail.com>
2025-05-22 22:00:18 +0800

    feat(nixvim): add all snacks picker keybindings
    - and fix description

--- home/bosse/common/core/nvim/plugins/which-key/picker.nix
+++ home/bosse/common/core/nvim/plugins/which-key/picker.nix
@@ -1,4 +1,3 @@
-# TODO: fix desc and check unused picker
 [
   # picker
   {
@@ -44,15 +43,15 @@
     mode = "n";
   }
   {
-    __unkeyed = "<leader><space>l";
+    __unkeyed = "<leader><space>L";
     __unkeyed-1 = "<cmd>lua Snacks.picker.lines()<cr>";
-    desc = "Find String Under Cursor In CWD";
+    desc = "Find Lines";
     mode = "n";
   }
   {
     __unkeyed = "<leader><space>q";
     __unkeyed-1 = "<cmd>lua Snacks.picker.qflist()<cr>";
-    desc = "Find String Under Cursor In CWD";
+    desc = "Find Quickfix List";
     mode = "n";
   }
   {
@@ -82,31 +81,141 @@
   {
     __unkeyed = "<leader><space>.";
     __unkeyed-1 = "<cmd>lua Snacks.picker.icons()<cr>";
-    desc = "Find Todos";
+    desc = "Find Icons";
     mode = "n";
   }
   {
     __unkeyed = "<leader><space>n";
     __unkeyed-1 = "<cmd>lua Snacks.picker.notifications()<cr>";
-    desc = "Find Todos";
+    desc = "Find Notifications";
     mode = "n";
   }
   {
     __unkeyed = "<leader><space>:";
     __unkeyed-1 = "<cmd>lua Snacks.picker.command_history()<cr>";
-    desc = "Find Todos";
+    desc = "Find Command History";
     mode = "n";
   }
   {
     __unkeyed = "<leader><space>/";
     __unkeyed-1 = "<cmd>lua Snacks.picker.search_history()<cr>";
-    desc = "Find Todos";
+    desc = "Find Search History";
     mode = "n";
   }
   {
-    __unkeyed = "<leader><space>''\"";
+    __unkeyed = ''<leader><space>"'';
     __unkeyed-1 = "<cmd>lua Snacks.picker.registers()<cr>";
-    desc = "Find Todos";
+    desc = "Find Registers";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>'";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.marks()<cr>";
+    desc = "Find Marks";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>j";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.jumps()<cr>";
+    desc = "Find Jumps";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>u";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.undo()<cr>";
+    desc = "Find Undo";
+    mode = "n";
+  }
+  # picker.git
+  {
+    __unkeyed = "<leader><space>g";
+    group = "Git";
+    icon = " ";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>gb";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.git_branches()<cr>";
+    desc = "Find Git Branches";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>gl";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.git_log()<cr>";
+    desc = "Find Git Log";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>gL";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.git_log_file()<cr>";
+    desc = "Find Git Log File";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>gs";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.git_status()<cr>";
+    desc = "Find Git Status";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>gS";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.git_stash()<cr>";
+    desc = "Find Git Stash";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>gd";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.git_diff()<cr>";
+    desc = "Find Git Diff (Hunks)";
+    mode = "n";
+  }
+  # picker.lsp
+  {
+    __unkeyed = "<leader><space>l";
+    group = "LSP";
+    icon = " ";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>ld";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.lsp_definitions()<cr>";
+    desc = "Find LSP Definitions";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>lD";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.lsp_declarations()<cr>";
+    desc = "Find LSP Declarations";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>lr";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.lsp_references()<cr>";
+    desc = "Find LSP References";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>li";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.lsp_implementations()<cr>";
+    desc = "Find LSP Implementations";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>lt";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.lsp_type_definitions<cr>";
+    desc = "Find LSP Type Definitions";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>ls";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.lsp_symbols()<cr>";
+    desc = "Find LSP Symbols";
+    mode = "n";
+  }
+  {
+    __unkeyed = "<leader><space>lS";
+    __unkeyed-1 = "<cmd>lua Snacks.picker.lsp_workspace_symbols()<cr>";
+    desc = "Find LSP Workspace Symbols";
     mode = "n";
   }
 ]
